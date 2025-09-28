b475b3b6da09a8b6a73d5bcaf58e1363544c0e66
Lu1s <lftalwys@gmail.com>
2025-09-29 01:39:43 +0800

    Added logout and placeholder for layout

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -14,7 +14,7 @@ class Home extends Controller
         }
         $placeholder = [
             'Content' => '',
-            'Title' => 'This'
+            'Title' => 'Car'
         ];
         //model
         
@@ -24,6 +24,29 @@ class Home extends Controller
         $layout = $this->get_layout("main");
         $placeholder['Content'] = $this->get_view("home/index.php");
         $this->view($this->set_placeholders($placeholder, $layout));
+    }    
+
+    public function logout() : void 
+    {
+        Application::session_destroy();
+        Application::redirect("/login");
     }
-    
+
+    public function landingpage() : void {
+        $placeholder = [
+            'pagetitle' => 'locomotive.com',
+            'landingpage' => ''
+
+        ];
+ 
+        $data = [
+            'landingpage' => '<h1>Welcome to Locomotive</h1><p>Your gateway to seamless travel experiences. 
+            Explore our services and book your next journey with us today!</p>'
+            ,'message' => 'supit si jepoy dizon'
+        ];
+
+        $layout = $this->get_layout("layout");
+        $placeholder["landingpage"] = $this->get_view("home/landingpage.php", $data);
+        $this->view($this->set_placeholders($placeholder, $layout));
+    }
 }
